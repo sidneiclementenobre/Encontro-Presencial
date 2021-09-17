47a51e84907ed4ab6c0c217a4990057d6cae676f
sidneiclementenobre <sidneiclementenobre@gmail.com>
2021-09-16 23:46:56 -0300

    enviar

---
+++ index.js
@@ -0,0 +1,18 @@
+var numero = 0
+var total = 14
+
+for(var contador = 0;contador<=total;contador++) {
+    if(contador == 0) {
+        console.log("O numero e ZERO");
+    }else{
+        if(contador%2 == 0){ 
+            console.log("O numero " +contador + " e PAR");
+        }else{
+            if(contador%2 ==1) {
+                console.log("O numero " +contador+ " e IMPAR");
+            }else{ 
+            }
+        }
+    }
+    
+}
